258151e28d48c72aba109f5f8bdd405d12a9bd4f
jan <jan.hofmeier@yahoo.de>
2014-12-16 21:01:29 +0100

    GUI gegeben

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,10 +3,39 @@ package gui;
 
 public class GUI{
 
+	private int aAktXFrosch;
+	private int aAktYFrosch;
+	
+	//Nur da um die Werte nicht Richard-Style hardzucoden
 	public final int xLength,yLength;
+	
 	public GUI(int xLength, int yLength) {
 		this.xLength=xLength;
 		this.yLength=yLength;
 	}
 
+	public void clickNeuesSpiel()
+	{
+		//TODO
+	}
+	
+	public void clickSpielBeenden()
+	{
+		//TODO
+	}
+	
+	public void loescheAlles()
+	{
+		//TODO
+	}
+	
+	public void zeichenBlatt(int pX, int pY)
+	{
+		//TODO
+	}
+	
+	public void zeichneMuenze(int pX, int pY)
+	{
+		//TODO
+	}
 }
