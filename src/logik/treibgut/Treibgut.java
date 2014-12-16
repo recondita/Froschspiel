596f41d610f50c7a8e51a8b20a0e5f285a14f684
jan <jan.hofmeier@yahoo.de>
2014-12-16 20:28:26 +0100

    Methoden Treibgut gegeben

--- src/logik/treibgut/Treibgut.java
+++ src/logik/treibgut/Treibgut.java
@@ -5,8 +5,23 @@ public class Treibgut {
 	protected int aX;
 	protected int aY;
 	
-	public Treibgut() {
-		// TODO Auto-generated constructor stub
+	public Treibgut(int aX, int aY) {
+		this.aX=aX;
+		this.aY=aY;
 	}
 
+	public int gibX()
+	{
+		return aX;
+	}
+	
+	public int giby()
+	{
+		return aY;
+	}
+	
+	public boolean hatGleichesXY(int pX, int pY)
+	{
+		return pX==this.aX&&pY==this.aY;
+	}
 }
