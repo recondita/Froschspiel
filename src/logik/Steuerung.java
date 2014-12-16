16ee847175568c2d6493ab38dd72e701d14d5bb2
jan <jan.hofmeier@yahoo.de>
2014-12-16 20:13:48 +0100

    Attribute Steuerung gegeben

--- src/logik/Steuerung.java
+++ src/logik/Steuerung.java
@@ -2,6 +2,11 @@ package logik;
 
 public class Steuerung {
 
+	private int aVersuche;
+	private int aMuenzzahl;
+	private int aBlattzahl;
+	private int aZustand;
+	
 	public Steuerung() {
 		// TODO Auto-generated constructor stub
 	}
