254558ee332379904d54a1025c0883a27f5e9b2a
jan <jan.hofmeier@yahoo.de>
2014-12-16 21:31:03 +0100

    Frosch Initialisieren

--- src/logik/treibgut/Frosch.java
+++ src/logik/treibgut/Frosch.java
@@ -1,9 +1,11 @@
 package logik.treibgut;
 
+import logik.Steuerung;
+
 public class Frosch extends Treibgut{
 //Diese Vererbung ist Schwachsinn
-	private int aVersuche;
-	private int aMuenzzahl;
+	private int aVersuche=Steuerung.START_VERSUCHE;
+	private int aMuenzzah=Steuerung.ANZ_MUENZEN;
 	
 	public Frosch(int pX, int pY) {
 		super(pX,pY);
