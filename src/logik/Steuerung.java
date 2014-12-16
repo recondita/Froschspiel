9639132af9e3a5e81789f52e96f4f4e8e3a28e86
jan <jan.hofmeier@yahoo.de>
2014-12-16 21:30:02 +0100

    Mehr Konstanten

--- src/logik/Steuerung.java
+++ src/logik/Steuerung.java
@@ -12,6 +12,8 @@ public class Steuerung {
 	//Ganz hardcoden wollte ich das jetzt nicht in den anderen Klassen... 
 	public final static int X_LENGTH=20;
 	public final static int Y_LENGTH=10;
+	public final static int START_VERSUCHE=3;
+	public final static int ANZ_MUENZEN=10;
 	
 	public Steuerung() {
 		new GUI(X_LENGTH,Y_LENGTH);
