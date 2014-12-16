d673c608d4f2f9c244ce84f317de2b54bf2ad520
jan <jan.hofmeier@yahoo.de>
2014-12-16 23:40:36 +0100

    GUI-Kram

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -2,43 +2,51 @@ package gui;
 
 import gui.oberflaeche.Fenster;
 
-
-public class GUI{
+public class GUI {
 
 	private int aAktXFrosch;
 	private int aAktYFrosch;
-	
-	//Nur da um die Werte nicht Richard-Style hardzucoden
-	public final int xLength,yLength;
-	
+
+	// Nur da um die Werte nicht Richard-Style hardzucoden
+	public final int xLength, yLength;
+
 	public GUI(int xLength, int yLength) {
-		this.xLength=xLength;
-		this.yLength=yLength;
+		this.xLength = xLength;
+		this.yLength = yLength;
 		new Fenster(this);
 	}
 
-	public void clickNeuesSpiel()
-	{
-		//TODO
+	public void clickNeuesSpiel() {
+		// TODO
 	}
-	
-	public void clickSpielBeenden()
-	{
+
+	public void clickSpielBeenden() {
 		System.exit(0);
 	}
-	
-	public void loescheAlles()
-	{
-		//TODO
+
+	public void loescheAlles() {
+		// TODO
 	}
-	
-	public void zeichenBlatt(int pX, int pY)
-	{
-		//TODO
+
+	public void zeichenBlatt(int pX, int pY) {
+		// TODO
 	}
-	
-	public void zeichneMuenze(int pX, int pY)
-	{
-		//TODO
+
+	public void zeichneMuenze(int pX, int pY) {
+		// TODO
+	}
+
+	public void zeichneFrosch(int pX, int pY) {
+		loescheFrosch();
+		aAktXFrosch=pX;
+		aAktYFrosch=pY;
+		//TODO GUI Kram
+	}
+
+	public void loescheFrosch() {
+		if (aAktXFrosch >= 0&&aAktXFrosch>=0) {
+			// TODO: gui Kram
+			aAktXFrosch = aAktYFrosch = -1;
+		}
 	}
 }
