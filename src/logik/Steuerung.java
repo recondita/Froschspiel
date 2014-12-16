54e4f5465de767e2711418f6e1364001cee93e43
jan <jan.hofmeier@yahoo.de>
2014-12-16 20:44:37 +0100

    länge und breite

--- src/logik/Steuerung.java
+++ src/logik/Steuerung.java
@@ -7,6 +7,10 @@ public class Steuerung {
 	private int aBlattzahl;
 	private int aZustand;
 	
+	//Ganz hardcoden wollte ich das jetzt nicht in den anderen Klassen... 
+	public final static int X_LENGTH=20;
+	public final static int Y_LENGTH=10;
+	
 	public Steuerung() {
 		// TODO Auto-generated constructor stub
 	}
