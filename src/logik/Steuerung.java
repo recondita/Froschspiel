371d54d97bf42295d543c131ea58f0ffffdd2a6b
jan <jan.hofmeier@yahoo.de>
2014-12-16 22:28:18 +0100

    Steuerung bekommt GUI und erstellt sie nicht.

--- src/logik/Steuerung.java
+++ src/logik/Steuerung.java
@@ -4,10 +4,14 @@ import gui.GUI;
 
 public class Steuerung {
 
+	//gegeben
 	private int aVersuche;
 	private int aMuenzzahl;
 	private int aBlattzahl;
 	private int aZustand;
+	
+	//aus den aufgaben
+	private GUI aGUI;
 	
 	//Ganz hardcoden wollte ich das jetzt nicht in den anderen Klassen... 
 	public final static int X_LENGTH=20;
@@ -15,12 +19,12 @@ public class Steuerung {
 	public final static int START_VERSUCHE=3;
 	public final static int ANZ_MUENZEN=10;
 	
-	public Steuerung() {
-		new GUI(X_LENGTH,Y_LENGTH);
+	public Steuerung(GUI pGUI) {
+		this.aGUI=pGUI;
 	}
 
 	public void main(String[] args)
 	{
-		new Steuerung();
+		new Steuerung(new GUI(X_LENGTH,Y_LENGTH));
 	}
 }
